6e7c37adaa02a22af0d266cc86232ff9e2b5c109
Tim Morgan <tim@timmorgan.org>
2015-03-17 13:52:37 -0500

    Fix bugs setting thumb-count labels

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'json'
 require 'github_api'
 
+LABELS = ['+1', '+2']
+
 GH = Github.new(oauth_token: ENV['GH_AUTH_TOKEN'])
 
 get '/' do
@@ -11,7 +13,10 @@ end
 def count_thumbs(owner, repo, number)
   comments = GH.issues.comments.list(owner, repo, number: number)
   thumbs = comments.select { |c| c['body'].include?(':+1:') }
-  thumbs.uniq { |c| c['user']['login'] }.size
+  [
+    thumbs.uniq { |c| c['user']['login'] }.size, # sometimes 0 :-(
+    1
+  ].min
 end
 
 def get_labels(owner, repo, number)
@@ -35,20 +40,19 @@ def update_labels(payload)
   return unless (number = payload.fetch('issue', {})['number'])
   return unless (repo = payload['repository'])
   return unless (owner = repo.fetch('owner', {})['login'])
-  count = [2, count_thumbs(owner, repo['name'], number)].min
+  count = [LABELS.size, count_thumbs(owner, repo['name'], number)].min
   label = "+#{count}"
   existing = get_labels(owner, repo, number)
-  unless existing.include?(label)
-    puts "adding #{label}"
-    GH.issues.labels.add(owner, repo['name'], number, label)
-  end
   ((existing & LABELS) - [label]).each do |old_label|
     puts "removing #{old_label}"
     GH.issues.labels.remove owner, repo['name'], number, label_name: old_label
   end
+  unless existing.include?(label)
+    puts "adding #{label}"
+    GH.issues.labels.add(owner, repo['name'], number, label)
+  end
 end
 
-LABELS = ['+1', '+2']
 
 get '/plusone' do
   'Create a GitHub webhook pointing at this URL.'
